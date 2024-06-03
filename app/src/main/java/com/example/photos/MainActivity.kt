bc794c426fe1e4f448e2e3f79b264f157dbee454
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-02 23:00:56 -1200

    Add Dependency

--- app/src/main/java/com/example/photos/MainActivity.kt
+++ app/src/main/java/com/example/photos/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PhotosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
