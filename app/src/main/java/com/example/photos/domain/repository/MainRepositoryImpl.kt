8ffdfe336a2ff2972d96beaa60d257144abe6750
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-06 07:25:48 -1200

    Create Repository Impl

--- app/src/main/java/com/example/photos/domain/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/example/photos/domain/repository/MainRepositoryImpl.kt
@@ -2,9 +2,11 @@ package com.example.photos.domain.repository
 
 import com.example.photos.data.remote.ApiService
 import com.example.photos.domain.model.PhotoResponse
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class MainRepositoryImpl @Inject constructor(private val apiService: ApiService) : MainRepository {
@@ -17,7 +19,7 @@ class MainRepositoryImpl @Inject constructor(private val apiService: ApiService)
             delay(3000)
         }
 
-    }
+    }.flowOn(Dispatchers.IO)
 
 
 }
