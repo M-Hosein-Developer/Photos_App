2ae3201bec25c55952f00b11c25352693e3d29c3
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-06 07:50:21 -1200

    Setup UseCase

--- app/src/main/java/com/example/photos/domain/usecase/MainUseCase.kt
+++ app/src/main/java/com/example/photos/domain/usecase/MainUseCase.kt
@@ -1,4 +1,10 @@
 package com.example.photos.domain.usecase
 
+import com.example.photos.domain.model.PhotoResponse
+import kotlinx.coroutines.flow.Flow
+
 interface MainUseCase {
+
+    suspend fun getPhotos() : PhotoResponse
+
 }
--- app/src/main/java/com/example/photos/domain/usecase/MainUseCaseImpl.kt
+++ app/src/main/java/com/example/photos/domain/usecase/MainUseCaseImpl.kt
@@ -1,8 +1,17 @@
 package com.example.photos.domain.usecase
 
+import com.example.photos.domain.model.PhotoResponse
+import com.example.photos.domain.repository.MainRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
-class MainUseCaseImpl @Inject constructor() : MainUseCase {
+class MainUseCaseImpl @Inject constructor(private val mainRepository: MainRepository) : MainUseCase {
 
+    override suspend fun getPhotos(): PhotoResponse = mainRepository.getPhotos()
 
 }
