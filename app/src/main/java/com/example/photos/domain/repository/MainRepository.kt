1d9e2b5105b62e52003585a8284dfc6ddee439fd
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-06 07:24:11 -1200

    Create Repository

--- app/src/main/java/com/example/photos/domain/repository/MainRepository.kt
+++ app/src/main/java/com/example/photos/domain/repository/MainRepository.kt
@@ -1,4 +1,12 @@
 package com.example.photos.domain.repository
 
+import android.provider.Contacts
+import com.example.photos.domain.model.PhotoResponse
+import kotlinx.coroutines.flow.Flow
+
 interface MainRepository {
+
+
+    val getPhotos : Flow<PhotoResponse>
+
 }
--- app/src/main/java/com/example/photos/domain/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/example/photos/domain/repository/MainRepositoryImpl.kt
@@ -1,9 +1,23 @@
 package com.example.photos.domain.repository
 
+import com.example.photos.data.remote.ApiService
+import com.example.photos.domain.model.PhotoResponse
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class MainRepositoryImpl @Inject constructor() : MainRepository {
+class MainRepositoryImpl @Inject constructor(private val apiService: ApiService) : MainRepository {
 
+
+    override val getPhotos: Flow<PhotoResponse> = flow {
+
+        while (true){
+            emit(apiService.getPhotos("cHrEYuOkjgsNihjYNRpvMpRQsw3PZxKY1jBGXRol3mc"))
+            delay(3000)
+        }
+
+    }
 
 
 }
