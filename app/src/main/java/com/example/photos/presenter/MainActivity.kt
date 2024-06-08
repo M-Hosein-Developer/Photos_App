9f24621aa21ebe19cc4b488c9dcc2d1978506146
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-07 23:12:41 -1200

    Debugging Main Activity Code

--- app/src/main/java/com/example/photos/presenter/MainActivity.kt
+++ app/src/main/java/com/example/photos/presenter/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -29,13 +30,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             PhotosTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-
-
-
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    MainUi()
                 }
             }
         }
@@ -45,7 +46,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainUi(){
 
-    Surface(
+    Column(
         modifier = Modifier.fillMaxSize()
     ) {
 
