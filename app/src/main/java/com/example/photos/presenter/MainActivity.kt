e4b2beea3f7a61d53365b82bfedbea100c3fbf11
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-06-08 07:03:55 -1200

    Add View Model

--- app/src/main/java/com/example/photos/presenter/MainActivity.kt
+++ app/src/main/java/com/example/photos/presenter/MainActivity.kt
@@ -3,17 +3,12 @@ package com.example.photos.presenter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -36,7 +31,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ){
-                    MainUi()
+                    MainUi(viewModel)
                 }
             }
         }
@@ -44,26 +39,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainUi(){
+fun MainUi(viewModel: MainViewModel) {
 
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
 
-        NavScreen()
+        NavScreen(viewModel)
 
     }
 }
 
 @Composable
-fun NavScreen(){
+fun NavScreen(viewModel: MainViewModel) {
 
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = MyScreen.PhotoScreen.route){
 
         composable(MyScreen.PhotoScreen.route){
-            PhotoScreen()
+            PhotoScreen(viewModel)
         }
 
     }
